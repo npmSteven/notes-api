26096c81ed19f452ff971758116f98633dd7c5c1
BuildTools <unconfigured@null.spigotmc.org>
2020-10-23 10:42:47 +0100

    remove console.log

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -136,8 +136,6 @@ router.post('/register', async (req, res) => {
         });
     }
 
-    console.log(JSON.stringify(newUser, null, 2));
-
     return res.status(200).json({
       success: true,
       payload: {
